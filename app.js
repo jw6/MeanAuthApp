f0fd1496b2db157bad5aada80aab8f0bc9fa85b1
Jay Wang <jaywang007@yahoo.com>
2018-01-31 21:31:41 -0600

    setup entry file and connected to database

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
 const express = require('express');
 const path = require('path');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to databse
+mongoose.connect(config.database);
+
+mongoose.connection.on('error', (err) => {
+  console.log('database error: ' + config.database);
+});
 
 const app = express();
 
+const users = require('./routes/users')
+
 const port = 3000;
 
+app.use(cors());
+
+/*
+Body Parse Middleware
+Parse incoming request body
+*/
+app.use(bodyParser.json());
+/*
+Get ExpressJS module from 
+https://enable-cors.org/server_expressjs.html
+*/
+
+app.use('/users', users);
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Index routes
 app.get('/', (req, res) => {
   res.send('Invalid Endpoint');
 });
 
+//Start server
 app.listen(port, () => {
   console.log('Server started on port ' + port);
 });
