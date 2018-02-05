7eb74dc0adb915e37dc9b1060d0e58c7202d9ac2
Jay Wang <jaywang007@yahoo.com>
2018-02-04 18:04:59 -0600

    setup navbar

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-
+const routes : Routes = [
+  {path:'', component: HomeComponent},
+  {path:'register', component: RegisterComponent },
+  {path:'dashboard', component: DashboardComponent},
+  {path:'login', component: LoginComponent},
+  {path:'profile', component: ProfileComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(routes, {useHash: true})],
   ],
   providers: [],
   bootstrap: [AppComponent]
