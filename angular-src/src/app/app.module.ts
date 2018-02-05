af8c6d7d7c6c4ca205162df7ea51f809726a0d86
Jay Wang <jaywang007@yahoo.com>
2018-02-04 23:21:56 -0600

    add register form

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -30,6 +31,7 @@ const routes : Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     [RouterModule.forRoot(routes, {useHash: true})],
   ],
   providers: [],
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+  
   constructor() { }
 
   ngOnInit() {
