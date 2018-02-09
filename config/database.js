80013048c790b59386750201af083ad7e0ae0af0
Jay Wang <jaywang007@yahoo.com>
2018-02-09 15:25:27 -0600

    fix link

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   // database: 'mongodb://localhost:27017/meanauth',
-  database: mongodb://admin:p4ssw0rd@ds231228.mlab.com:31228/testsandbox
+  database: 'mongodb://admin:p4ssw0rd@ds231228.mlab.com:31228/testsandbox'
   secret: 'yoursecret'
 }
