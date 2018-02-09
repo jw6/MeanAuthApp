763da7ec1e4943029006018de202dc62a84a99c6
Jay Wang <jaywang007@yahoo.com>
2018-02-08 22:59:04 -0600

    add auth guard

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -28,6 +29,10 @@ export class AuthService {
     localStorage.setItem('user', JSON.stringify(user));
     this.authToken = token;
     this.user      = user;
+  }
+
+  loggedIn() {
+    return tokenNotExpired('id_token');
   }
 
   logout() {
