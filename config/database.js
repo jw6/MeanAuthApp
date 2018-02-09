ef7b5330d99a9368faa3b603abdf0b3bbbba72bd
Jay Wang <jaywang007@yahoo.com>
2018-02-09 15:18:06 -0600

    add mlab connection

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  // database: 'mongodb://localhost:27017/meanauth',
+  database: mongodb://superadmin:p4ssw0rd@ds137729.mlab.com:37729/startapp
   secret: 'yoursecret'
 }
